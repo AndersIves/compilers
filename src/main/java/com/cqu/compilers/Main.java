f222e10c21a56c145d00b92f6506ce5d0d64d6fc
andersives <13895637677@126.com>
2019-09-25 21:49:50 +0800

    123

--- src/main/java/com/cqu/compilers/Main.java
+++ src/main/java/com/cqu/compilers/Main.java
@@ -6,14 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("[");
-        for (int i = 0;i < args.length;i++) {
-            if (i != 0) {
-                System.out.print(",");
-            }
-            System.out.print("\"" + args[i] + "\"");
-        }
-        System.out.println("]");
         List<String> list = FileUtils.File2String(args[0]);
         System.out.println(list);
     }
