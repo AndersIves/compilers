0dec04dc10b0f1c8336d55d26dbeca7cd610e4f0
andersives <13895637677@126.com>
2019-09-25 22:00:57 +0800

    last commit

--- src/main/java/com/cqu/compilers/Main.java
+++ src/main/java/com/cqu/compilers/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> list = FileUtils.File2String(args[0]);
+
         for (String s : list) {
             System.out.println(s);
         }
